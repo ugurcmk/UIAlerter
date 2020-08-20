2a75a8314128ddce11248b128cc8a4d724b60c15
Uğur Çakmak <ugurcmk@gmail.com>
2020-08-20 11:07:39 +0300

    Initial commit

---
+++ UIAlerter/UIAlerter/UIAlerter.h
@@ -0,0 +1,19 @@
+//
+//  UIAlerter.h
+//  UIAlerter
+//
+//  Created by ugur cakmak on 20.08.2020.
+//  Copyright © 2020 ugur cakmak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIAlerter.
+FOUNDATION_EXPORT double UIAlerterVersionNumber;
+
+//! Project version string for UIAlerter.
+FOUNDATION_EXPORT const unsigned char UIAlerterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIAlerter/PublicHeader.h>
+
+
