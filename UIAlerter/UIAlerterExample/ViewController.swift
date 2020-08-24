437b0311c1c8c77877463cafdb06f3982252c36e
Uğur Çakmak <ugurcmk@gmail.com>
2020-08-24 23:08:08 +0300

    Delete boilerplate code

--- UIAlerter/UIAlerterExample/ViewController.swift
+++ UIAlerter/UIAlerterExample/ViewController.swift
@@ -12,21 +12,7 @@ import UIAlerter
 class ViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
-        //UIAlerterBuilder().setBackgroundColor(UIColor.gray).show()
-        
-//        let alert = UIAlerterBuilder().setBackgroundColor(UIColor.lightGray)
-//        alert.layer.opacity = 0.5
-//        
-//        UIApplication.shared.keyWindow?.addSubview(alert)
-//        
-//        UIView.animate(withDuration: 0.3, delay:0.2, options: [.curveEaseInOut], animations: {
-//            alert.frame.origin.y = 0
-//            alert.layer.opacity = 1
-//        }, completion: { _ in
-//            UIView.animate(withDuration: 0.3, delay: 0, options: [.curveEaseIn], animations: {
-//                alert.frame.origin.y = alert.frame.origin.y - 30
-//            }, completion: nil)
-//        })
+        UIAlerterBuilder().setBackgroundColor(UIColor.lightGray).show()
     }
 }
 
